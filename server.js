bfd4d11678748c2768c50d12adffe27ee9d2c4e2
Kolby Law <kolbytlaw@gmail.com>
2021-01-28 14:17:16 -0700

    Updated Heroku Port.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -41,7 +42,7 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-  
+
 app.get('/api/animals', (req, res) => {
 let results = animals;
 if (req.query) {
@@ -50,6 +51,6 @@ if (req.query) {
 res.json(results);
 });
  
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
